8ac78e651e6c570cbb2255e9d8fa2178d1ac27f1
LeonardoTrapani <leonard.trapani@gmail.com>
2023-03-26 16:15:15 +0200

    fix: check if the topic of the feedback is of the same user as the feedback itself

--- src/server/api/routers/feedbacks.ts
+++ src/server/api/routers/feedbacks.ts
@@ -6,41 +6,55 @@ import { z } from "zod"
 import { TRPCError } from "@trpc/server";
 
 export const feedbacksRouter = createTRPCRouter({
+
   getAll: protectedProcedure.query(async ({ ctx }) => {
     return await ctx.prisma.feedback.findMany(({
       where: {
+        userId: ctx.session.user.id,
         topic: {
           userId: ctx.session.user.id
         }
       }
     }))
   }),
+
   create: protectedProcedure.input(z.object({
     title: z.string().nullish(),
     content: z.string(),
     topicId: z.string()
   })).mutation(async ({ ctx, input }) => {
-    const feedbacksTopic = await ctx.prisma.topic.findUnique({ where: { id: input.topicId } }) //the topic of our feedback
-    if (feedbacksTopic?.userId !== ctx.session.user.id) {
-      throw new TRPCError({
-        code: 'INTERNAL_SERVER_ERROR',
-        message: 'This topic is not yours'
-      })
-    }
+    const feedbacksTopic = await ctx.prisma.topic.findFirst({where: {
+      id: input.topicId,
+      userId: ctx.session.user.id
+    }})
+    if (!feedbacksTopic) throw new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR', 
+      message: "The selected topic isn't yours"
+    })
     return await ctx.prisma.feedback.create({
       data: {
         title: input.title,
         content: input.content,
         topicId: input.topicId,
+        userId: ctx.session.user.id
       },
     })
   }),
+
   edit: protectedProcedure.input(z.object({
     id: z.string(),
     newTitle: z.string().nullish(),
     newContent: z.string().nullish(),
     newTopicId: z.string().nullish(),
   })).mutation(async ({ ctx, input }) => {
+    const feedbacksTopic = await ctx.prisma.topic.findFirst({where: {
+      id: input.newTopicId || undefined,
+      userId: ctx.session.user.id
+    }})
+    if (!feedbacksTopic) throw new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR', 
+      message: "The selected topic isn't yours"
+    })
     return await ctx.prisma.feedback.update({
       where: {
         id: input.id
@@ -52,14 +66,17 @@ export const feedbacksRouter = createTRPCRouter({
       }
     })
   }),
+
   delete: protectedProcedure.input(z.object({
     id: z.string(),
   })).mutation(async ({ ctx, input }) => {
-    return await ctx.prisma.feedback.delete({
+    return await ctx.prisma.feedback.deleteMany({
       where: {
-        id: input.id
+        id: input.id,
+        userId: ctx.session.user.id
       },
     })
   })
+
 });
 
