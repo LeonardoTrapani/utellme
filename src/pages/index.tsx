b668b0d6724679c53560a3f5bea12875b3c7161f
LeonardoTrapani <leonard.trapani@gmail.com>
2023-03-26 11:46:57 +0200

    added feedback and topics fields on prisma schema

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Tell Me</title>
         <meta name="description" content="a web app to get feedback" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
