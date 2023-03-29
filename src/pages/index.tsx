970f08b5db7e460929dc6f77ff000a92fb3a54fb
LeonardoTrapani <leonard.trapani@gmail.com>
2023-03-29 20:06:13 +0200

    feat: added new project button and changed menu selected item color to accent

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,6 +39,7 @@ const MainPageContent: React.FC = () => {
       <Navbar />
       <body className="flex items-stretch p-4">
         <ul className="menu bg-base-200 w-56 p-2 rounded-box">
+          <li><button className="btn btn-primary mb-2">New Project</button></li>
           {
             projectsData?.map((project, i) => {
               return <ProjectComponent key={i} project={project} isActive={selectedProjectIndex === i} onPress={onProjectPress} index={i} />
@@ -57,12 +58,31 @@ const MainPageContent: React.FC = () => {
   )
 }
 
-const ProjectComponent: React.FC<{ project: Project, isActive: boolean, onPress: (i: number) => void; index: number }> = (props) => {
-  return <li key={props.project.id}><a className={`${props.isActive ? "active" : ""}`} onClick={() => props.onPress(props.index)}>{props.project.name}</a></li>
+const ProjectComponent: React.FC<{
+  project: Project,
+  isActive: boolean, onPress: (i: number) => void; index: number
+}> = (props) => {
+  // className={`${props.isActive ? "btn-accent text-neutral-200" : ""} hover:bg-accent hover:opacity-90 hover:text-neutral-200`} to put in a
+  return (
+    <li key={props.project.id}>
+      <a
+        className={`${props.isActive ? "btn-accent text-neutral-200" : ""} active:bg-accent`}
+        onClick={() => props.onPress(props.index)}
+      >
+        {props.project.name}
+      </a>
+    </li>
+  )
 }
 
 const FeedbackComponent: React.FC<{ feedback: Feedback }> = (props) => {
-  return <li key={props.feedback.id}><a>{props.feedback.content}</a></li>
+  return (
+    <li key={props.feedback.id}>
+      <a>
+        {props.feedback.content}
+      </a>
+    </li>
+  )
 }
 
 const LoginPage: React.FC = () => {
