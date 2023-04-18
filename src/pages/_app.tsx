edce7ff23fee587042de871dcc79dc5275883814
LeonardoTrapani <leonard.trapani@gmail.com>
2023-04-18 17:27:33 +0200

    feat: cookie consent hook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,11 +10,14 @@ import { Toaster } from "react-hot-toast";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import Script from "next/script";
+import { useCookieConsent } from "~/utils/hooks";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const [cookieConsent, setCookieConsent] = useCookieConsent();
+
   const router = useRouter();
 
   useEffect(() => {
@@ -29,6 +32,12 @@ const MyApp: AppType<{ session: Session | null }> = ({
 
   return (
     <>
+      {
+        !cookieConsent && (
+          <div className="">
+          </div>
+        )
+      }
       <GoogleAnalytics googleAnalyticsId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || ""} />
       <GoogleAdsense />
       <Toaster toastOptions={{
@@ -85,3 +94,6 @@ export const GoogleAnalytics: React.FC<{
 }
 
 export default api.withTRPC(MyApp);
+
+// In order to give you the best experience, we and our third party partners may use cookies and similar technologies, for example, to analyze usage and optimize our sites and services, personalize content, tailor and measure our marketing and to keep the site secure. Please visit our privacy notice for more information and our cookies notice for a list of all cookies used. Our Cookies Notice
+
