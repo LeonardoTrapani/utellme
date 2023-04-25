569d845561fea77642b6b9784462beb46b7b132c
LeonardoTrapani <leonard.trapani@gmail.com>
2023-04-25 19:55:02 +0200

    feat: added trpc ssr

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -19,7 +19,19 @@ const getBaseUrl = () => {
 
 /** A set of type-safe react-query hooks for your tRPC API. */
 export const api = createTRPCNext<AppRouter>({
-  config() {
+  config({ ctx }) {
+    if (typeof window !== "undefined") {
+      // during client requests
+      return {
+        transformer: superjson, // optional - adds superjson serialization
+        links: [
+          httpBatchLink({
+            url: '/api/trpc',
+          }),
+        ],
+      };
+    }
+
     return {
       /**
        * Transformer used for data de-serialization from the server.
@@ -39,9 +51,29 @@ export const api = createTRPCNext<AppRouter>({
             process.env.NODE_ENV === "development" ||
             (opts.direction === "down" && opts.result instanceof Error),
         }),
-        httpBatchLink({
-          url: `${getBaseUrl()}/api/trpc`,
-        }),
+        httpBatchLink(
+          {
+            // The server needs to know your app's full url
+            url: `${getBaseUrl()}/api/trpc`,
+            /**
+             * Set custom request headers on every request from tRPC
+             * @link https://trpc.io/docs/v10/header
+             */
+            headers() {
+              if (!ctx?.req?.headers) {
+                return {};
+              }
+              // To use SSR properly, you need to forward the client's headers to the server
+              // This is so you can pass through things like cookies when we're server-side rendering
+              const {
+                // If you're using Node 18 before 18.15.0, omit the "connection" header
+                connection: _connection,
+                ...headers
+              } = ctx.req.headers;
+              return headers;
+            },
+          }
+        ),
       ],
     };
   },
@@ -50,7 +82,7 @@ export const api = createTRPCNext<AppRouter>({
    *
    * @see https://trpc.io/docs/nextjs#ssr-boolean-default-false
    */
-  ssr: false,
+  ssr: true,
 });
 
 /**
