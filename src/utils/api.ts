dfe4d7de9f55ca077c6bcc55f121c7749651ac8f
LeonardoTrapani <leonard.trapani@gmail.com>
2023-05-06 14:54:07 +0200

    feat: removed ssr

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -51,29 +51,9 @@ export const api = createTRPCNext<AppRouter>({
             process.env.NODE_ENV === "development" ||
             (opts.direction === "down" && opts.result instanceof Error),
         }),
-        httpBatchLink(
-          {
-            // The server needs to know your app's full url
-            url: `${getBaseUrl()}/api/trpc`,
-            /**
-             * Set custom request headers on every request from tRPC
-             * @link https://trpc.io/docs/v10/header
-             */
-            headers() {
-              if (!ctx?.req?.headers) {
-                return {};
-              }
-              // To use SSR properly, you need to forward the client's headers to the server
-              // This is so you can pass through things like cookies when we're server-side rendering
-              const {
-                // If you're using Node 18 before 18.15.0, omit the "connection" header
-                connection: _connection,
-                ...headers
-              } = ctx.req.headers;
-              return headers;
-            },
-          }
-        ),
+        httpBatchLink({
+          url: `${getBaseUrl()}/api/trpc`,
+        }),
       ],
     };
   },
@@ -82,7 +62,7 @@ export const api = createTRPCNext<AppRouter>({
    *
    * @see https://trpc.io/docs/nextjs#ssr-boolean-default-false
    */
-  ssr: true,
+  ssr: false,
 });
 
 /**
