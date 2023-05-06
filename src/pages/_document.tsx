71d6e66ff587ca9bb0b128373859e98a05a403c5
LeonardoTrapani <leonard.trapani@gmail.com>
2023-05-06 14:49:01 +0200

    feat: updated favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -34,8 +34,15 @@ export default class MyDocument extends Document {
 export const FaviconScripts = () => {
   return (
     <>
-      <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
-      <link rel="icon" type="image/png" href="/favicons/favicon.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+      <link rel="manifest" href="/favicons/site.webmanifest" />
+      <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#eab308" />
+      <link rel="shortcut icon" href="/favicons/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#eab308" />
+      <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
+      <meta name="theme-color" content="#ffffff" />
     </>
   )
 };
