f45344bac4cfe22bb8132ca62ba52fbe46aabcf1
LeonardoTrapani <leonard.trapani@gmail.com>
2023-05-06 15:20:10 +0200

    feat: removed unused var

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -19,7 +19,7 @@ const getBaseUrl = () => {
 
 /** A set of type-safe react-query hooks for your tRPC API. */
 export const api = createTRPCNext<AppRouter>({
-  config({ ctx }) {
+  config() {
     if (typeof window !== "undefined") {
       // during client requests
       return {
