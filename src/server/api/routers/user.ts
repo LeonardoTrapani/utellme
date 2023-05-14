81886e7e15db7e6782daeec7f7499e47a31a3545
LeonardoTrapani <leonard.trapani@gmail.com>
2023-05-14 16:43:51 +0200

    feat: delete stripe on account delete

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -3,6 +3,7 @@ import {
   protectedProcedure,
 } from "~/server/api/trpc";
 import { z } from "zod"
+import { stripe } from "~/server/stripe/client";
 
 export const userRouter = createTRPCRouter({
 
@@ -20,14 +21,26 @@ export const userRouter = createTRPCRouter({
   }),
 
   deleteAccount: protectedProcedure.mutation(async ({ ctx }) => {
-    return await ctx.prisma.user.delete({
-      where: {
-        id: ctx.session.user.id,
-      },
-      include: {
-        sessions: true,
-        accounts: true,
+    return await ctx.prisma.$transaction(async (trx) => {
+      const user = await trx.user.findUnique({
+        where: {
+          id: ctx.session.user.id,
+        },
+      });
+      if (!user) throw new Error("Could not find user");
+      await trx.user.delete({
+        where: {
+          id: ctx.session.user.id,
+        },
+        include: {
+          sessions: true,
+          accounts: true,
+        }
+      });
+      if (user.stripeCustomerId) {
+        await stripe.customers.del(user.stripeCustomerId)
       }
+      return;
     });
   }),
 
